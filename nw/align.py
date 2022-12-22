9c005cf42c6087f7d0a9f0c1e06609f49b1b1a80
Valeriia Zakharova <54675260+Zyxkim@users.noreply.github.com>
2022-12-22 15:38:57 +0300

    Update align.py

--- nw/align.py
+++ nw/align.py
@@ -1,6 +1,36 @@
 import copy
 import itertools
 from sys import stdout
+import time
+import argparse
+
+class Fasta:
+    def __init__(self):
+        self.name = ""
+        self.sequence = ""
+
+
+def read_fasta(file):
+    name, seq = None, []
+    for line in file:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if name: yield name, ''.join(seq)
+            name, seq = line, []
+        else:
+            seq.append(line)
+    if name: yield name, ''.join(seq)
+
+
+def parse(file_name):
+    res = []
+    with open(file_name) as f:
+        for name, seq in read_fasta(f):
+            record = Fasta()
+            record.name = name
+            record.sequence = seq
+            res.append(record)
+    return res
 
 
 def sigma(a, b):
@@ -76,3 +106,35 @@ def NW(a, b, gap=-5):
     score_num = D[n][m]
 
     return "".join(align_a).rstrip('*').strip('*'), "".join(align_b).rstrip('*').strip('*'), score_num
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', nargs='+', type=str)
+    parser.add_argument('-o', type=str)
+    parser.add_argument('-g', type=int)
+    args = parser.parse_args()
+
+    if len(args.i) == 1:
+        records = parse(args.i[0])
+        start = time.time()
+        a, b, s = NW(records[0].sequence, records[1].sequence, args.g)
+        end = time.time()
+        print(end - start)
+        with open(args.o, "w") as f:
+            print(s, file=f)
+            write_to_file("> out sequence 1", a, f)
+            write_to_file("> out sequence 2", b, f)
+    elif len(args.i) == 2:
+        records = parse(args.i[0]) + parse(args.i[1])
+        start = time.time()
+        a, b, s = NW(records[0].sequence, records[1].sequence, args.g)
+        end = time.time()
+        print(end - start)
+
+        with open(args.o, "w") as f:
+            print(s, file=f)
+            write_to_file("> out sequence 1", a, f)
+            write_to_file("> out sequence 2", b, f)
+    else:
+        print('Too many files!')
